ce5897239938593e811edc067e7d1b374ae38ceb
FatherAbraham <damianspendel@Damians-iMac.local>
2014-07-24 19:05:20 +0200

    Updated script

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## example usage
+## my_matrix <- matrix(c(1,2,12,13), nrow =2, ncol=2)
+## testmatrix <- makeCacheMatrix(my_matrix)
+## cacheSolve(testmatrix)
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-
-...yet to add...
-
+makeCacheMatrix <- function(x = numeric(), rows, cols) {
+	m <- NULL
+	set <- function(y) { 
+		x <<- y
+		m <<- NULL
+	}
+	get <- function() x
+	setsolve <- function() m
+	list(set = set, get = get,
+		setsolve = setsolve,
+		getsolve = getsolve)
 }
 
+##operates the class makeCacheMatrix using the cacheSolve function methods, namely solve the (square, invertible) matrix
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'my_matrix'
+
+	m <- x$getsolve()
+
+	if(!is.null(m)) {
+		message("getting cached matrix")
+		return(m)
+	}
+	matr <- x$get()
+	m <- solve(matr, ...)
+	x$setsolve(m)
+	m
+
 }
